fc55627ef811767b6c70f38a31cd31980df37dba
Andre Tirado <andre8tr@gmail.com>
2025-11-11 15:39:52 -0600

    new model for adoptant

---
+++ src/models/adopterModel.ts
@@ -0,0 +1,30 @@
+import { Schema, model, Document, Types } from 'mongoose'
+
+//Interface de adoptante
+export interface IAdopter extends Document {
+  name: string
+  address: string
+  email?: string
+  phone: string
+  pets: Types.ObjectId[]
+  createdAt: Date
+  updatedAt: Date
+}
+
+const adopterSchema = new Schema<IAdopter>(
+  {
+    name: { type: String, required: true, trim: true },
+    email: { type: String, required: false, unique: true, lowercase: true },
+    phone: { type: String, required: true },
+    address: { type: String },
+    pets: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Pet', // 👈 referencia al modelo Pet
+      },
+    ],
+  },
+  { timestamps: true }
+)
+
+export const Adopter = model<IAdopter>('Adopter', adopterSchema, 'adopters')
